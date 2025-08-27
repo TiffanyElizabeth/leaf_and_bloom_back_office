88546492891ba3b0c145e6a50099e36ce1f78943
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-08-27 14:42:40 +0200

    first commit

---
+++ src/main/java/final_project/leaf_and_bloom_back_office/LeafAndBloomBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package final_project.leaf_and_bloom_back_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafAndBloomBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafAndBloomBackOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/final_project/leaf_and_bloom_back_office/LeafAndBloomBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package final_project.leaf_and_bloom_back_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafAndBloomBackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
