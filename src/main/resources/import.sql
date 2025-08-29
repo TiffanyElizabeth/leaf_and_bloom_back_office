28f5f5f872b5b23a68ba040fda894471b87ae5f5
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-08-29 12:19:08 +0200

    updated import data file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name, photo_url) VALUES ('White', NULL);
+INSERT INTO categories (name, photo_url) VALUES ('Green', NULL);
+INSERT INTO categories (name, photo_url) VALUES ('Oolong', NULL);
+INSERT INTO categories (name, photo_url) VALUES ('Black', NULL);
+INSERT INTO categories (name, photo_url) VALUES ('Herbal', NULL);
+INSERT INTO categories (name, photo_url) VALUES ('Rooibis', NULL);
+
+
+
