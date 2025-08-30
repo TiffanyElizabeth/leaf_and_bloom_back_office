9aacdaf7c4ca1448bc8e331ddd19d635b5215484
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-08-30 11:47:00 +0200

    changed home to redirect to teas index

--- src/main/java/final_project/leaf_and_bloom_back_office/controller/HomeController.java
+++ src/main/java/final_project/leaf_and_bloom_back_office/controller/HomeController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping
 public class HomeController {
     @GetMapping
     public String home(Model model) {
-        return "home";
+        return "redirect:/teas";
     }
 
 }
