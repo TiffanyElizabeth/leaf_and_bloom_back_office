82ac9c72fdf6e4bfbca24c99e70b48fc12469add
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-19 10:54:14 +0200

    add explanations to TeaRestController

--- src/main/java/final_project/leaf_and_bloom_back_office/controller/TeaRestController.java
+++ src/main/java/final_project/leaf_and_bloom_back_office/controller/TeaRestController.java
@@ -1,22 +1,25 @@
 package final_project.leaf_and_bloom_back_office.controller;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.List; // used to return multiple teas/categories
+import java.util.Optional; // used when retrieving a single tea by ID (may or may not exist)
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired; // allows Spring to inject service objects here (dependency injection)
+import org.springframework.http.HttpStatus; // http response codes 
+import org.springframework.http.ResponseEntity; // allows us to return data + status codes 
+import org.springframework.web.bind.annotation.RequestMapping; // sets a base path for all routes in this controller
+import org.springframework.web.bind.annotation.RestController; // exposes endpoints returning json responses
+import org.springframework.web.bind.annotation.CrossOrigin; // allows requests from different domains (here, to allow React FE access)
+import org.springframework.web.bind.annotation.GetMapping; // maps HTTP GET request to a method
+import org.springframework.web.bind.annotation.PathVariable; // extracts values from the UR
+L
+import org.springframework.web.bind.annotation.RequestParam; // extracts query parameters from the URL
 
 import final_project.leaf_and_bloom_back_office.model.*;
 import final_project.leaf_and_bloom_back_office.service.CategoryService;
 import final_project.leaf_and_bloom_back_office.service.TeaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/teas")
 public class TeaRestController {
     @Autowired
@@ -40,11 +43,11 @@ public class TeaRestController {
     public ResponseEntity<Tea> show(@PathVariable Integer id) {
         Optional<Tea> teaAttempt = teaService.findById(id);
         if (teaAttempt.isEmpty()) {
-            return new ResponseEntity<Tea>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<Tea>(HttpStatus.NOT_FOUND); // returns 404 NOT FOUND if the tea doesn't exist
 
         }
 
-        return new ResponseEntity<Tea>(teaAttempt.get(), HttpStatus.OK);
+        return new ResponseEntity<Tea>(teaAttempt.get(), HttpStatus.OK); // else returns 200 OK & the tea object
     }
 
     @GetMapping("/categories")
